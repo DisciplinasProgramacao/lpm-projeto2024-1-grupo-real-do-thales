2fcf27a3a19b655a6b4070f73965e8b26971fa1b
gabitolage <gglage11@gmail.com>
2024-05-13 09:25:03 -0300

    Aplicando springboot

---
+++ realthales/src/main/java/com/example/realthales/RealthalesApplication.java
@@ -0,0 +1,13 @@
+package com.example.realthales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealthalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealthalesApplication.class, args);
+	}
+
+}
---
+++ realthales/src/test/java/com/example/realthales/RealthalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realthales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealthalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
