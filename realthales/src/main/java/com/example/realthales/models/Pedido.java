18d619a4ee342ad885399ddae985a1975c63a491
gabitolage <gglage11@gmail.com>
2024-05-13 10:02:47 -0300

    Pedido e requisição DisciplinasProgramacao/lpm-projeto2024-1-grupo-real-do-thales#5 Pedido

--- realthales/src/main/java/com/example/realthales/models/Pedido.java
+++ realthales/src/main/java/com/example/realthales/models/Pedido.java
@@ -13,12 +13,12 @@ public class Pedido {
         produtos = new ArrayList<>();
     }
 
-      // Método para adicionar um produto ao pedido
+
       public void adicionarProduto(Produto produto) {
         produtos.add(produto);
     }
 
-    // Método para calcular o valor total do pedido
+
     public void calcularValorTotal() {
         double valorTotal = 0.0;
         for (Produto produto : produtos) {
